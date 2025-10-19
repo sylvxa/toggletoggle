c4e430d4eff78cd27901432b82e8dfbc202f7e97
sylvie 3 <me@sylvie.lol>
2025-10-19 12:26:38 -0500

    update to 1.21.9/10

--- src/main/java/lol/sylvie/tt/ToggleToggle.java
+++ src/main/java/lol/sylvie/tt/ToggleToggle.java
@@ -9,14 +9,18 @@ import net.minecraft.client.option.SimpleOption;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 
 public class ToggleToggle implements ClientModInitializer {
     private static KeyBinding sprintKeyBinding;
     private static KeyBinding sneakKeyBinding;
 
-    private void toggleOption(ClientPlayerEntity player, SimpleOption<Boolean> option) {
+    private static final KeyBinding.Category keybindingCategory = KeyBinding.Category.create(Identifier.of("toggletoggle", "keys"));
+
+    private void toggleOption(ClientPlayerEntity player, SimpleOption<Boolean> option, KeyBinding keybind) {
         boolean newValue = !option.getValue();
         option.setValue(newValue);
+        if (!newValue) keybind.setPressed(false);
 
         if (player == null) return;
         player.sendMessage(Text.translatable("message.toggletoggle", option.toString(), Text.translatable(newValue ? "message.toggletoggle.enabled" : "message.toggletoggle.disabled").formatted(Formatting.YELLOW)), true);
@@ -28,19 +32,23 @@ public class ToggleToggle implements ClientModInitializer {
                 "key.toggletoggle.sprint",
                 InputUtil.Type.KEYSYM,
                 InputUtil.UNKNOWN_KEY.getCode(),
-                "category.toggletoggle.keys"
+                keybindingCategory
         ));
 
         sneakKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.toggletoggle.sneak",
                 InputUtil.Type.KEYSYM,
                 InputUtil.UNKNOWN_KEY.getCode(),
-                "category.toggletoggle.keys"
+                keybindingCategory
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (sprintKeyBinding.wasPressed()) toggleOption(client.player, client.options.getSprintToggled());
-            while (sneakKeyBinding.wasPressed()) toggleOption(client.player, client.options.getSneakToggled());
+            while (sprintKeyBinding.wasPressed())
+                toggleOption(client.player, client.options.getSprintToggled(), client.options.sprintKey);
+
+            while (sneakKeyBinding.wasPressed())
+                toggleOption(client.player, client.options.getSneakToggled(), client.options.sneakKey);
+
         });
     }
 }
