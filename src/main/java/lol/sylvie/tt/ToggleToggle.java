c126c2970373d5ec32bfee67a0090827a5cc69d1
sylvie 3 <me@sylvie.lol>
2025-12-14 11:18:31 -0600

    mojmap, 1.21.11

--- src/main/java/lol/sylvie/tt/ToggleToggle.java
+++ src/main/java/lol/sylvie/tt/ToggleToggle.java
@@ -1,53 +1,53 @@
 package lol.sylvie.tt;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.option.SimpleOption;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 
 public class ToggleToggle implements ClientModInitializer {
-    private static KeyBinding sprintKeyBinding;
-    private static KeyBinding sneakKeyBinding;
+    private static KeyMapping sprintKeyBinding;
+    private static KeyMapping sneakKeyBinding;
 
-    private static final KeyBinding.Category keybindingCategory = KeyBinding.Category.create(Identifier.of("toggletoggle", "keys"));
+    private static final KeyMapping.Category keybindingCategory = KeyMapping.Category.register(Identifier.fromNamespaceAndPath("toggletoggle", "keys"));
 
-    private void toggleOption(ClientPlayerEntity player, SimpleOption<Boolean> option, KeyBinding keybind) {
-        boolean newValue = !option.getValue();
-        option.setValue(newValue);
-        if (!newValue) keybind.setPressed(false);
+    private void toggleOption(LocalPlayer player, OptionInstance<Boolean> option, KeyMapping keybind) {
+        boolean newValue = !option.get();
+        option.set(newValue);
+        if (!newValue) keybind.setDown(false);
 
         if (player == null) return;
-        player.sendMessage(Text.translatable("message.toggletoggle", option.toString(), Text.translatable(newValue ? "message.toggletoggle.enabled" : "message.toggletoggle.disabled").formatted(Formatting.YELLOW)), true);
+        player.displayClientMessage(Component.translatable("message.toggletoggle", option.toString(), Component.translatable(newValue ? "message.toggletoggle.enabled" : "message.toggletoggle.disabled").withStyle(ChatFormatting.YELLOW)), true);
     }
 
     @Override
     public void onInitializeClient() {
-        sprintKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        sprintKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.toggletoggle.sprint",
-                InputUtil.Type.KEYSYM,
-                InputUtil.UNKNOWN_KEY.getCode(),
+                InputConstants.Type.KEYSYM,
+                InputConstants.UNKNOWN.getValue(),
                 keybindingCategory
         ));
 
-        sneakKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        sneakKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.toggletoggle.sneak",
-                InputUtil.Type.KEYSYM,
-                InputUtil.UNKNOWN_KEY.getCode(),
+                InputConstants.Type.KEYSYM,
+                InputConstants.UNKNOWN.getValue(),
                 keybindingCategory
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (sprintKeyBinding.wasPressed())
-                toggleOption(client.player, client.options.getSprintToggled(), client.options.sprintKey);
+            while (sprintKeyBinding.consumeClick())
+                toggleOption(client.player, client.options.toggleSprint(), client.options.keySprint);
 
-            while (sneakKeyBinding.wasPressed())
-                toggleOption(client.player, client.options.getSneakToggled(), client.options.sneakKey);
+            while (sneakKeyBinding.consumeClick())
+                toggleOption(client.player, client.options.toggleCrouch(), client.options.keyShift);
 
         });
     }
